cc9af4990dabfa7e28d41881665a68b1d0b67d51
Ildevert Daisy <ildevertdaisy@gmail.com>
2024-03-21 22:03:14 +0100

    Second commit

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 
 app = Flask(__name__)
 
+# Définir le dossier où les fichiers téléchargés seront stockés
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+# Assurez-vous que le dossier existe
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/partager", methods=["GET"])
+def upload():
+    return render_template("upload.html")
+
+
+@app.route("/upload/files", methods=["POST"])
+def upload_file():
+    if 'file' not in request.files:
+        return "Aucun fichier part"
+    
+    file = request.files["file"]
+    if file.filename == '':
+        return 'Aucun fichier sélectionné'
+    
+    if file:
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return 'Fichier téléchargé avec succès'
 
 
 if __name__ == "__main__":
